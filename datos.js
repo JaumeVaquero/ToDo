cb4401d6bf77c3a534a1b08aa08c2cfaf805fad4
DESKTOP-5OCFC6R\jaume <jaumevaquero@gmail.com>
2021-02-03 17:35:45 +0100

    rehaciendo todo

--- datos.js
+++ datos.js
@@ -11,78 +11,20 @@ const printContact = function (pTareasList) {
 }
 
 
-//pintar solo los laborales.
-function filterContactsByCategory(pCategory, pTareasList) {
+//pintar solo los filtrados.
+function filterContactsByPriority(pPriority, pTareasList) {
 
-    const listaFiltrada = pTareasList.filter(contact => tarea.category == pCategory)
+    const listaFiltrada = pTareasList.filter(priority => priority.prioridad == pPriority)
 
     return listaFiltrada;
 }
 
-//quiero sacar por consola todos los contactos masculinos
-
-function filterByGender(pListaContactos, pGender) {
-
-    let listaContactosGender = pListaContactos.filter((contacto) => contacto.gender == pGender)
-
-    return listaContactosGender;
-}
-
-
 
 //pintar toda la lista de contactos
-printContact(contactos);
+printTareas(tareas);
 
 let selectTypeContact = document.querySelector('#tipo');
 
 selectTypeContact.addEventListener('change', getType);
 
 function getType(event) {
-    //todos los elementos de tipo formulario recoge su valor con un propiedad value
-
-    let tipo = event.target.value;
-    if (tipo != "") {
-        let lista = filterContactsByCategory(tipo, contactos);
-        printContact(lista);
-    } else {
-        printContact(contactos);
-    }
-
-}
-
-//quiero un filtro selector por genero masculino , y femenino.
-
-let selectGender = document.querySelector('#gender');
-selectGender.addEventListener('change', getGender);
-
-function getGender(event) {
-    let gender = event.target.value;
-    if (gender != "") {
-        let lista = filterByGender(contactos, gender);
-        printContact(lista);
-    } else {
-        printContact(contactos);
-    }
-}
-
-//quiero la busqueda por palabra
-let inputNombre = document.querySelector('#nombre');
-let btnBusqueda = document.querySelector('#btn');
-
-btnBusqueda.addEventListener('click', getInputData);
-inputNombre.addEventListener('keydown', getInputData); //enter
-
-function getInputData(event) {
-    if (event.keyCode == 13 || event.type == 'click') {
-        let palabraBuscar = inputNombre.value;
-        let listaPersonas = searchByWord(palabraBuscar, contactos);
-        printContact(listaPersonas);
-    }
-}
-
-function searchByWord(pWord, pContactList) {
-    const filterList = pContactList.filter(contact => {
-        return contact.name.toLowerCase().includes(pWord.toLowerCase());
-    })
-    return filterList
-}
--- listaTareas.js
+++
@@ -1,5 +0,0 @@
-var listaTareas = new Array();
-listaTareas = [{
-    idTarea':0, 'titulo':, 'Estudiar Javascript', 'prioridad': 'urgente'},
-    
-}]
